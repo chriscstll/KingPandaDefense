29c0ae20861a9066aa030ba3e13fbf2f6f3bb04d
chriscstll <castilloxtiann@gmail.com>
2026-09-04 16:54:13 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 
+const abstracts = path.resolve(import.meta.dirname, 'src/scss/abstracts').replace(/\\/g, '/')
 export default defineConfig({
   base: '/KingPandaDefense/',
+
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
+    }
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api', 'import', 'color-functions', 'global-builtin'],
+        additionalData: `@use "${abstracts}" as *;`
+      }
     }
   },
 
